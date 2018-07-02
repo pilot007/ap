f641b28682aa320e7fa9eac3282aecccb75c271f
Oğuzhan Yapıcı <yapici.oguzhan@gmail.com>
2018-07-02 16:00:40 +0300

    Initial Commit

---
+++ asum_deneme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  asum_deneme
+//
+//  Created by Oğuzhan Yapıcı on 2.07.2018.
+//  Copyright © 2018 Oğuzhan Yapıcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
