043ec188049e10db1bcfd444132cf5a4959e7357
Selim Goktas <selimgoktas@gmail.com>
2018-08-03 23:52:49 +0300

    ...

---
+++ asum_deneme/Controllers/LoggedStoryBoard/profileVC.swift
@@ -0,0 +1,21 @@
+//
+//  profileVC.swift
+//  asum_deneme
+//
+//  Created by Oğuzhan Yapıcı on 24.07.2018.
+//  Copyright © 2018 Oğuzhan Yapıcı. All rights reserved.
+//
+
+import UIKit
+
+class profileVC: BaseMenuController {
+    
+    @IBOutlet var openMenu: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
